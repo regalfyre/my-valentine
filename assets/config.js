31aa4648635ffa7fb265e10e2ac4111866b6fdd3
regalfyre <125704424+regalfyre@users.noreply.github.com>
2026-02-14 14:04:22 -0600

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 export const ATLAS = {
   WORKER: "https://valentine-worker.regalfyre.workers.dev",
-  PREFIX: "",   // or whatever folder path exists inside the R2 bucket
+  PREFIX: "cards/",
   PAGE_LIMIT: 1000
 };
